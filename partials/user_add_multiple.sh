5a10c25f55cbce89faf6c1b60b37b5c841bf68c7
Nathan Clark <Nathan.Clark@nasa.gov>
2015-10-30 11:13:19 -0400

    Notifying that a restart is needed.

--- partials/user_add_multiple.sh
+++ partials/user_add_multiple.sh
@@ -9,7 +9,6 @@ while [ ! -z "$answer" ]; do
 		if [ -e ~/.ssh/authorized_keys ]; then
 			echo "Copying authorized keys for $answer"
 			home_dir=`eval echo "~$answer"`
-			echo "into $home_dir"
 			mkdir -p "$home_dir/.ssh"
 			cp ~/.ssh/authorized_keys "$home_dir/.ssh/"
 			chown -R "$answer:$answer" "$home_dir/.ssh/"
